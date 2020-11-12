5d64462a9880b0a80105b7da576db3cb36b71803
thiago <thiagod.smartins@icloud.com>
2020-11-12 16:17:50 -0400

    função que remove ambiguidade de anchors para o mesmo plano

--- Planos/ViewController.swift
+++ Planos/ViewController.swift
@@ -23,16 +23,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Do any additional setup after loading the view.
     }
 
-
+    //é chamado quando um novo plano horizontal é detectado
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
         print("new flat surface detected, new ARPlaneAnchor added")
     }
     
+    //é chamado quando um plano é mudado - tamanho
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
-        print("new flat surface detected, new ARPlaneAnchor added")
+        
         print("updationg floor anchor")
+    }
+    // é chamado quando é criado mais de um anchor pro mesmo plano, é nesse momento que ele remove um deles e a ambiguidade é cancelada
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        print("I made a mistake, and I am fixing it...")
     }
 }
 
