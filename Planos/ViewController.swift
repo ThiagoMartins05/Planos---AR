e1bc3ef54621dbc6aac993e011f6380ca7d45bbd
thiago <thiagod.smartins@icloud.com>
2020-11-12 15:47:06 -0400

    configurações iniciais

--- Planos/ViewController.swift
+++ Planos/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [.showWorldOrigin, .showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view.
     }
 
 
+    
 }
 
