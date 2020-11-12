986cc54bc5546958daac79ac16b5c33a84bada23
thiago <thiagod.smartins@icloud.com>
2020-11-12 16:41:34 -0400

    adicionei a imagem de lava aos dois lados do plano

--- Planos/ViewController.swift
+++ Planos/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func createLava() -> SCNNode{
         let lavaNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
         lavaNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "lava")
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
         lavaNode.position = SCNVector3(0,0,-1)
         lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
         return lavaNode
