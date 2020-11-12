7226d80b4401a8bc4e911de433c9f2f165940a50
thiago <thiagod.smartins@icloud.com>
2020-11-12 16:06:18 -0400

    detecção de planos horizontais

--- Planos/ViewController.swift
+++ Planos/ViewController.swift
@@ -19,10 +19,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.sceneView.debugOptions = [.showWorldOrigin, .showFeaturePoints]
         self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
         // Do any additional setup after loading the view.
     }
 
 
-    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("new flat surface detected, new ARPlaneAnchor added")
+    }
 }
 
